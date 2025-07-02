01e5babafc509d591e0e39919d0b3be0994a5066
Yungyingzerza <yungyingzerza2547@gmail.com>
2025-07-03 00:08:47 +0700

    edit cors

--- index.ts
+++ index.ts
@@ -15,7 +15,11 @@ import browseRouter from "./controllers/browse.routes";
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://skillswap.yungying.com",
+    ],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
