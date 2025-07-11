58dd1aa9b14a5648bf2aa91f3048548e09de4330
Yungyingzerza <yungyingzerza2547@gmail.com>
2025-07-11 20:33:18 +0700

    fix cors in socket

--- socket/socket.ts
+++ socket/socket.ts
@@ -1,20 +1,21 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
 // const socketIo = require('socket.io');
-import { Server } from 'socket.io';
-import http from 'http';
+import { Server } from "socket.io";
+import http from "http";
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
     origin: [
-      'http://localhost:3000',
-      'http://127.0.0.1:3000'
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://skillswap.yungying.com",
     ],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   },
 });
@@ -22,31 +23,28 @@ const io = new Server(server, {
 // Users store: userId (string) → array of socket ids (string[])
 const users: Record<string, string[]> = {};
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
+  socket.on("join", (data) => {
+    // users[data.id] = socket.id;
+    //one user can have multiple socket connections
+    if (!users[data.id]) {
+      users[data.id] = [];
+    }
 
-    socket.on('join', (data) => {
-        // users[data.id] = socket.id;
-        //one user can have multiple socket connections
-        if (!users[data.id]) {
-            users[data.id] = [];
-        }
-
-        users[data.id].push(socket.id);
-    });
-
+    users[data.id].push(socket.id);
+  });
 });
 
 server.listen(process.env.WS_PORT, () => {
-    console.log(`Server_WS is running on port ${process.env.WS_PORT}`);
+  console.log(`Server_WS is running on port ${process.env.WS_PORT}`);
 });
-
 
 function getSocketId(userId: string): string[] | undefined {
   return users[userId];
 }
 
 function getAllUsers() {
-    return users;
+  return users;
 }
 
 export { io, getSocketId, app, getAllUsers };
